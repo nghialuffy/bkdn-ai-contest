0018281e98c0cf5c2edde317bb90383898121446
Nguyễn Văn Đại <37271151+nguyenvandai61@users.noreply.github.com>
2021-04-11 03:58:13 +0700

    add some documents
    Admin manage database directly

--- ai_contest_website/ai_contest_website/urls.py
+++ ai_contest_website/ai_contest_website/urls.py
@@ -18,6 +18,18 @@ from django.contrib import admin
 from django.urls import include, path
 from django.urls import path
 
+from api.models.User import User
+from api.models.Language import Language
+from api.models.Contest import Contest
+from api.models.Problem import Problem
+from api.models.Result import Result
+
+admin.site.register(User)
+admin.site.register(Language)
+admin.site.register(Contest)
+admin.site.register(Problem)
+admin.site.register(Result)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls'))
