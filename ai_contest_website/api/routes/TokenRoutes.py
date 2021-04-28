36a20b422e979afbc35c92037762e3227a97f22d
nguyenvandai61 <nguyenvandai61@users.noreply.github.com>
2021-04-28 15:56:04 +0700

    Refresh token after 5 minutes

--- ai_contest_website/api/routes/TokenRoutes.py
+++ ai_contest_website/api/routes/TokenRoutes.py
@@ -3,6 +3,6 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
     # ex: /polls/
-    path('refresh', TokenRefreshView.as_view, name='refresh_token'),
+    path('refresh', TokenRefreshView.as_view(), name='refresh_token'),
     # path('<int:pk>/', language_api.detail_item, name='detail'),
 ]
