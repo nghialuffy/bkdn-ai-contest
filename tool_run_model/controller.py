3eff4fcff4d683e20dbbf4970936b8011e945f88
DEV_DN_NGHIA\DEV_NGHIALV <nghialv@tmtsofts.com>
2021-04-29 08:49:17 +0700

    add language c++, python, javascript

--- tool_run_model/controller.py
+++ tool_run_model/controller.py
@@ -1,41 +1,94 @@
 from data_access import *
 from variable import *
-import shutil, os
-
+import shutil, os, subprocess
+from datetime import datetime
 def excute_code(file_path, language, code_test_name_file, code_train_name_file, input_file, output_file):
-    os.chdir(file_path)
-    if language == "python2":
-        # active_venv
-        output_test = os.popen(f"python {code_test_name_file}").read()
-        print(output_test)
-        output_train = os.popen(f"python {code_train_name_file}").read()
-        print(output_train)
-    elif language == "python3":
-        # load result_file
+    try:
+        os.chdir(file_path)
+        # Load result
         data_output = open(os.path.join(file_path, output_file), 'r').readlines()
         data_output = [x.replace("\n","").strip() for x in data_output if x != "" or x != None]
-        # run code_train
-        output_train = os.popen(f"{PYTHON3_VENV} {code_train_name_file}").read()
-        print("Training...")
-        print(output_train)
-        # run code_test
-        output_test = os.popen(f"{PYTHON3_VENV} {code_test_name_file} {input_file}").read()
-        output_test = [x.replace("\n", "").strip() for x in output_test.split("\n")]
-        output_test.remove("")
-        count = 0
-        for res, excute in tuple(zip(data_output, output_test)):
-            if str(res) == str(excute):
-                count += 1
-        accuracy = round(float(count/len(data_output)), 5)
-        print(accuracy)
-        return accuracy
-    elif language == "javascript":
-        output_test = os.popen(f"node {code_test_name_file}").read()
-        print(output_test)
-        output_train = os.popen(f"node {code_train_name_file}").read()
-        print(output_train)
-    return None
+        # Load input
+        data_input = open(os.path.join(file_path, input_file), 'r').readlines()
+        data_input = [x.replace("\n","").strip() for x in input_file if x != "" or x != None]
+        # excute
+        if language == "python2":
+            count = 0
+            list_test = []
+            for inp, out in tuple(zip(data_input, data_output)):
+                test_info = {}
+                ttime = datetime.now().timestamp()
+                output_test = subprocess.run(["python", code_test_name_file], stdout = subprocess.PIPE, text=True, input=inp)
+                ttime = datetime.now().timestamp() - ttime
+                test_info["time_excute"] = str(ttime)
+                if str(out) == str(output_test.stdout).replace("\n","").strip():
+                    test_info["status"] = True
+                    count += 1
+                else:
+                    test_info["status"] = False
+                list_test.append(test_info)
+            print(list_test)
+            accuracy = round(float(count/len(data_output)), 5)
+            return (accuracy, list_test)
+        elif language == "python3":
+            count = 0
+            list_test = []
+            for inp, out in tuple(zip(data_input, data_output)):
+                test_info = {}
+                ttime = datetime.now().timestamp()
+                output_test = subprocess.run([PYTHON3_VENV, code_test_name_file], stdout = subprocess.PIPE, text=True, input=inp)
+                ttime = datetime.now().timestamp() - ttime
+                test_info["time_excute"] = str(ttime)
+                if str(out) == str(output_test.stdout).replace("\n","").strip():
+                    test_info["status"] = True
+                    count += 1
+                else:
+                    test_info["status"] = False
+                list_test.append(test_info)
+            print(list_test)
+            accuracy = round(float(count/len(data_output)), 5)
+            return (accuracy, list_test)
+        elif language == "c++":
+            count = 0
+            list_test = []
+            for inp, out in tuple(zip(data_input, data_output)):
+                test_info = {}
+                ttime = datetime.now().timestamp()
+                output_test = subprocess.run(["g++", code_test_name_file], stdout = subprocess.PIPE, text=True, input=inp)
+                ttime = datetime.now().timestamp() - ttime
+                test_info["time_excute"] = str(ttime)
+                if str(out) == str(output_test.stdout).replace("\n","").strip():
+                    test_info["status"] = True
+                    count += 1
+                else:
+                    test_info["status"] = False
+                list_test.append(test_info)
+            print(list_test)
+            accuracy = round(float(count/len(data_output)), 5)
+            return (accuracy, list_test)
         
+        elif language == "javascript":
+            count = 0
+            list_test = []
+            for inp, out in tuple(zip(data_input, data_output)):
+                test_info = {}
+                ttime = datetime.now().timestamp()
+                output_test = subprocess.run(["node", code_test_name_file], stdout = subprocess.PIPE, text=True, input=inp)
+                ttime = datetime.now().timestamp() - ttime
+                test_info["time_excute"] = str(ttime)
+                if str(out) == str(output_test.stdout):
+                    test_info["status"] = True
+                    count += 1
+                else:
+                    test_info["status"] = False
+                list_test.append(test_info)
+            print(list_test)
+            accuracy = round(float(count/len(data_output)), 5)
+            return (accuracy, list_test)
+    except Exception as exc:
+        print("Error in excute code: %s" % str(exc))
+    return (None, None)
+    
 def process_result():
     db = DataBase()
     result = db.get_result()
