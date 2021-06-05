e99697ce51e427e11ea94c48cdf4a721eaf30f7c
nghialuffy <nghialuffy@github.com>
2021-06-05 19:00:58 +0700

    nghialuffy: Modified folder upload

--- ai_contest_website/api/models/Result.py
+++ ai_contest_website/api/models/Result.py
@@ -4,19 +4,20 @@ from api.models import Contest, Language, Problem
 from ai_contest_website.settings import MEDIA_URL
 from os.path import join
 from django.core.files.storage import FileSystemStorage
+from datetime import datetime
 
-fs = FileSystemStorage(location=join(MEDIA_URL, 'result'))
-
-
+def get_result_directory_path(instance, filename):
+    time_file = str(int(instance.time_submit.timestamp()))
+    return "result/{0}/{1}/{2}/{3}".format(instance.problem._id , instance.created_user._id, time_file, filename)
 class Result(models.Model):
     _id = models.ObjectIdField()
     problem = models.ForeignKey("problem", on_delete=models.CASCADE)
     created_user = models.ForeignKey("user", on_delete=models.CASCADE, related_name='result_created_user')
-    model_file = models.FileField(storage=fs)
-    code_test = models.FileField(storage=fs)
-    code_train = models.FileField(storage=fs)
-    accuracy = models.FloatField()
     time_submit = models.DateTimeField(auto_now_add=True)
+    model_file = models.FileField(upload_to = get_result_directory_path)
+    code_test = models.FileField(upload_to = get_result_directory_path)
+    code_train = models.FileField(upload_to = get_result_directory_path)
+    accuracy = models.FloatField()
     language = models.ForeignKey('language', on_delete=models.CASCADE)
     status = models.TextField(default='N')
     time_excute = models.FloatField()
@@ -25,7 +26,6 @@ class Result(models.Model):
 
     def save(self, *args, **kwargs):
         super(Result, self).save(**kwargs)
-
 
 # print("Create result")
 
