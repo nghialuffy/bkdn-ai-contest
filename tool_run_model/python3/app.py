3a54e42f60e87314f82b3064a2e7d375e1451040
nghialuffy <nghialuffy@github.com>
2021-08-08 01:39:48 +0700

    update python3 and docker-compose.yml

--- tool_run_model/python3/app.py
+++ tool_run_model/python3/app.py
@@ -1,10 +1,3 @@
 from controller import process_result
-from time import sleep
 if __name__ =="__main__":
-    print("Running")
-    while True:
-        try:
-            process_result()
-            sleep(1)
-        except Exception as exc:
-            print("Error in main: %s" % str(exc))
+    process_result()
