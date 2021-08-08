612478f43d3fc90cb604bb9fa65ac6cecc6ef58b
nguyenvandai61 <nguyenvandai61@users.noreply.github.com>
2021-08-08 21:34:10 +0700

    Change page number pagination

--- ai_contest_website/server/settings.py
+++ ai_contest_website/server/settings.py
@@ -57,7 +57,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
        'rest_framework_simplejwt.authentication.JWTTokenUserAuthentication',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
 
